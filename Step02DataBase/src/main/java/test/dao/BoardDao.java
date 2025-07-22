783ba32f702c8a5789bf4358b1510d2e20cfeec2
minaoh <ohmina0123@naver.com>
2025-07-22 16:57:09 +0900

    Step02DataBase 에 Board 검색 기능 구현

--- Step02DataBase/src/main/java/test/dao/BoardDao.java
+++ Step02DataBase/src/main/java/test/dao/BoardDao.java
@@ -18,11 +18,11 @@ public class BoardDao {
 	}
 	//외부에서 객체 생성하지 못하도록 생성자의 접근 지정자를 private 로 설정
 	private BoardDao() {}
+	
 	//참조값을 리턴해주는 static 메소드 제공
 	public static BoardDao getInstance() {
 		return dao;
 	}
-	
 	//특정 글의 조회수를 증가 시키는 메소드
 	public boolean addViewCount(int num) {
 		Connection conn = null;
@@ -32,10 +32,10 @@ public class BoardDao {
 		try {
 			conn = new DbcpBean().getConn();
 			String sql = """
-					UPDATE board
-					SET viewCount = viewCount+1
-					WHERE num=?
-					""";
+				UPDATE board
+				SET viewCount = viewCount+1
+				WHERE num=?
+			""";
 			pstmt = conn.prepareStatement(sql);
 			// ? 에 순서대로 필요한 값 바인딩
 			pstmt.setInt(1, num);
@@ -59,32 +59,31 @@ public class BoardDao {
 		} else {
 			return false; //작업 실패라는 의미에서 false 리턴하기
 		}
-		
 	}
-	
-	//전체 글의 갯수를 리턴하는 메소드
-	public int getCount() {
+
+	//검색 키워드에 부합하는 글의 갯수를 리턴하는 메소드
+	public int getCountByKeyword(String keyword) {
 		int count=0;
-		//필요한 객체를 담을 지역변수를 미리만든다
+		//필요한 객체를 담을 지역변수를 미리 만든다 
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
 			conn = new DbcpBean().getConn();
-			//실행할 sql 문
+			//실행할 sql문
 			String sql = """
-					SELECT MAX(ROWNUM) AS count
-					FROM board
-					""";
+				SELECT MAX(ROWNUM) AS count
+				FROM board
+				WHERE title LIKE '%' || ? || '%' OR content LIKE '%' || ? || '%'
+			""";
 			pstmt = conn.prepareStatement(sql);
-			//?에 값 바인딩
-
-			// select 문 실행하고 결과를 ResultSet으로 받아온다
+			//? 에 값 바인딩
+			pstmt.setString(1, keyword);
+			pstmt.setString(2, keyword);
+			// select 문 실행하고 결과를 ResultSet 으로 받아온다
 			rs = pstmt.executeQuery();
-			//반복문 돌면서 ResultSet 에 담긴 데이터를 추출해서 어떤 객체에 담는다
-			if(rs.next()) {
+			if (rs.next()) {
 				count=rs.getInt("count");
-
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -102,36 +101,76 @@ public class BoardDao {
 		return count;
 	}
 	
-	// 특정 page 에 해당하는 row 만 select 해서 리턴하는 메소드
-	// BoardDto 객체에 startRowNum 과 endRowNum 을 담아와서 select
-	public List<BoardDto> selectPage(BoardDto dto){
-		List<BoardDto> list=new ArrayList<>();
-		//필요한 객체를 담을 지역변수를 미리만든다
+	//전체 글의 갯수를 리턴하는 메소드
+	public int getCount() {
+		int count=0;
+		//필요한 객체를 담을 지역변수를 미리 만든다 
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
 			conn = new DbcpBean().getConn();
-			//실행할 sql 문
+			//실행할 sql문
 			String sql = """
-					SELECT *
-					FROM
-						(SELECT result1.*, ROWNUM AS rnum
-						FROM	
-							(SELECT num, writer, title, viewCount, createdAt
-							FROM board
-							ORDER BY num DESC) result1)
-					WHERE rnum BETWEEN ? AND ?		
-					""";
+				SELECT MAX(ROWNUM) AS count
+				FROM board
+			""";
 			pstmt = conn.prepareStatement(sql);
-			//?에 값 바인딩
-			pstmt.setInt(1, dto.getStartRowNum());
-			pstmt.setInt(2, dto.getEndRowNum());
-			// select 문 실행하고 결과를 ResultSet으로 받아온다
+			//? 에 값 바인딩
+
+			// select 문 실행하고 결과를 ResultSet 으로 받아온다
 			rs = pstmt.executeQuery();
-			//반복문 돌면서 ResultSet 에 담긴 데이터를 추출해서 어떤 객체에 담는다
+			if (rs.next()) {
+				count=rs.getInt("count");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return count;
+	}
+	// 특정 page 와 keyword 에 해당하는 row 만 select 해서 리턴하는 메소드
+	// BoardDto 객체에 startRowNum 과 endRowNum 을 담아와서 select 
+	public List<BoardDto> selectPageByKeyword(BoardDto dto){
+		List<BoardDto> list=new ArrayList<>();
+		
+		//필요한 객체를 담을 지역변수를 미리 만든다 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			//실행할 sql문
+			String sql = """
+				SELECT *
+				FROM
+					(SELECT result1.*, ROWNUM AS rnum
+					FROM	
+						(SELECT num, writer, title, viewCount, createdAt
+						FROM board
+						WHERE title LIKE '%' || ? || '%' OR content LIKE '%' || ? || '%'
+						ORDER BY num DESC) result1)
+				WHERE rnum BETWEEN ? AND ?
+			""";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 값 바인딩
+			pstmt.setString(1, dto.getKeyword());
+			pstmt.setString(2, dto.getKeyword());
+			pstmt.setInt(3, dto.getStartRowNum());
+			pstmt.setInt(4, dto.getEndRowNum());
+			// select 문 실행하고 결과를 ResultSet 으로 받아온다
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 ResultSet 에 담긴 데이터를 추출해서 리턴해줄 객체에 담는다
 			while (rs.next()) {
-				//객체 생성후
 				BoardDto dto2=new BoardDto();
 				dto2.setNum(rs.getInt("num"));
 				dto2.setWriter(rs.getString("writer"));
@@ -140,7 +179,6 @@ public class BoardDao {
 				dto2.setCreatedAt(rs.getString("createdAt"));
 				
 				list.add(dto2);
-
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -158,9 +196,60 @@ public class BoardDao {
 		return list;
 	}
 	
-	
-	
-	
+	// 특정 page 에 해당하는 row 만 select 해서 리턴하는 메소드
+	// BoardDto 객체에 startRowNum 과 endRowNum 을 담아와서 select 
+	public List<BoardDto> selectPage(BoardDto dto){
+		List<BoardDto> list=new ArrayList<>();
+		
+		//필요한 객체를 담을 지역변수를 미리 만든다 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			//실행할 sql문
+			String sql = """
+				SELECT *
+				FROM
+					(SELECT result1.*, ROWNUM AS rnum
+					FROM	
+						(SELECT num, writer, title, viewCount, createdAt
+						FROM board
+						ORDER BY num DESC) result1)
+				WHERE rnum BETWEEN ? AND ?
+			""";
+			pstmt = conn.prepareStatement(sql);
+			//? 에 값 바인딩
+			pstmt.setInt(1, dto.getStartRowNum());
+			pstmt.setInt(2, dto.getEndRowNum());
+			// select 문 실행하고 결과를 ResultSet 으로 받아온다
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 ResultSet 에 담긴 데이터를 추출해서 리턴해줄 객체에 담는다
+			while (rs.next()) {
+				BoardDto dto2=new BoardDto();
+				dto2.setNum(rs.getInt("num"));
+				dto2.setWriter(rs.getString("writer"));
+				dto2.setTitle(rs.getString("title"));
+				dto2.setViewCount(rs.getInt("viewCount"));
+				dto2.setCreatedAt(rs.getString("createdAt"));
+				
+				list.add(dto2);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return list;
+	}
 	
 	//글 수정하는 메소드
 	public boolean update(BoardDto dto) {
@@ -171,10 +260,10 @@ public class BoardDao {
 		try {
 			conn = new DbcpBean().getConn();
 			String sql = """
-					UPDATE board
-					SET title=?, content=?
-					WHERE num=?
-					""";
+				UPDATE board
+				SET title=?, content=?
+				WHERE num=?
+			""";
 			pstmt = conn.prepareStatement(sql);
 			// ? 에 순서대로 필요한 값 바인딩
 			pstmt.setString(1, dto.getTitle());
@@ -202,8 +291,6 @@ public class BoardDao {
 		}
 	}
 	
-	
-	
 	//글을 삭제하는 메소드
 	public boolean deleteByNum(int num) {
 		Connection conn = null;
@@ -213,9 +300,9 @@ public class BoardDao {
 		try {
 			conn = new DbcpBean().getConn();
 			String sql = """
-					DELETE FROM board
-					WHERE num=?
-					""";
+				DELETE FROM board
+				WHERE num=?
+			""";
 			pstmt = conn.prepareStatement(sql);
 			// ? 에 순서대로 필요한 값 바인딩
 			pstmt.setInt(1, num);
@@ -241,33 +328,29 @@ public class BoardDao {
 		}
 	}
 	
-	
-	
-	
 	//글 전체 목록을 리턴하는 메소드
 	public List<BoardDto> selectAll(){
 		//글목록을 담을 객체 생성
 		List<BoardDto> list=new ArrayList<>();
-		//필요한 객체를 담을 지역변수를 미리만든다
+		//필요한 객체를 담을 지역변수를 미리 만든다 
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
 			conn = new DbcpBean().getConn();
-			//실행할 sql 문
+			//실행할 sql문
 			String sql = """
-					SELECT num, writer, title, viewCount, createdAt
-					FROM board
-					ORDER BY num DESC
-					""";
+				SELECT num, writer, title, viewCount, createdAt
+				FROM board
+				ORDER BY num DESC
+			""";
 			pstmt = conn.prepareStatement(sql);
-			//?에 값 바인딩
+			//? 에 값 바인딩
 
-			// select 문 실행하고 결과를 ResultSet으로 받아온다
+			// select 문 실행하고 결과를 ResultSet 으로 받아온다
 			rs = pstmt.executeQuery();
-			//반복문 돌면서 ResultSet 에 담긴 데이터를 추출해서 어떤 객체에 담는다
+			//반복문 돌면서 ResultSet 에 담긴 데이터를 추출해서 리턴해줄 객체에 담는다
 			while (rs.next()) {
-				//객체 생성후
 				BoardDto dto=new BoardDto();
 				dto.setNum(rs.getInt("num"));
 				dto.setWriter(rs.getString("writer"));
@@ -276,7 +359,6 @@ public class BoardDao {
 				dto.setCreatedAt(rs.getString("createdAt"));
 				
 				list.add(dto);
-
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -293,9 +375,6 @@ public class BoardDao {
 		}
 		return list;
 	}
-	
-	
-	
 	
 	//글 하나의 정보를 리턴하는 메소드
 	public BoardDto getByNum(int num) {
@@ -334,7 +413,7 @@ public class BoardDao {
 				dto.setCreatedAt(rs.getString("createdAt"));
 				dto.setProfileImage(rs.getString("profileImage"));
 				dto.setPrevNum(rs.getInt("prevNum"));
-				dto.setNextNum(rs.getInt("nextNum"));				
+				dto.setNextNum(rs.getInt("nextNum"));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -432,4 +511,20 @@ public class BoardDao {
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- Step02DataBase/src/main/webapp/board/list.jsp
+++ Step02DataBase/src/main/webapp/board/list.jsp
@@ -1,54 +1,88 @@
-<%@page import="test.dao.BoardDao"%>
+<%@page import="org.apache.tomcat.jakartaee.commons.lang3.StringUtils"%>
 <%@page import="test.dto.BoardDto"%>
+<%@page import="test.dao.BoardDao"%>
 <%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%
-	//기본 페이지 번호는 1 로 설정하고
+	//검색 keyword 가 있는지 읽어와 본다.
+	String keyword=request.getParameter("keyword");
+	//System.out.println(keyword); // null or "" 또는 "검색어..." 
+	//keyword 가 만약 null 일 때 빈문자열로 출력되게 하면, null로 출력되는거 방지.
+	if(keyword==null){
+		keyword="";
+	}
+
+	//기본 페이지 번호는 1 로 설정하고 
 	int pageNum=1;
-	//페이지 번호를 읽어와서
+	//페이지 번호를 읽어와서 
 	String strPageNum=request.getParameter("pageNum");
-	//전달되는 페이지 번호가 있다면 (null이 아닌경우)
+	//전달되는 페이지 번호가 있다면 
 	if(strPageNum != null){
-		//해당 페이지 번호를 숫자로 변경해서 사용한다.
+		//해당 페이지 번호를 숫자로 변경해서 사용한다. 
 		pageNum=Integer.parseInt(strPageNum);
 	}
 	
-	//totalPageCount, PAGE_ROW_COUNT, PAGE_DISPLAY_COUNT 이 3가지를 계산해서 표시(한줄 한줄의 의미&원리 이해하기)
 	//한 페이지에 몇개씩 표시할 것인지
-	final int PAGE_ROW_COUNT=10;
+	final int PAGE_ROW_COUNT=3;
 	
 	//하단 페이지를 몇개씩 표시할 것인지
 	final int PAGE_DISPLAY_COUNT=3;
 
-	//보여줄 페이지의 시작 ROWNUM (일반식=글의 갯수 고려하지 않음.)
+	//보여줄 페이지의 시작 ROWNUM
 	int startRowNum=1+(pageNum-1)*PAGE_ROW_COUNT; //공차수열
 	//보여줄 페이지의 끝 ROWNUM
-	int endRowNum=pageNum*PAGE_ROW_COUNT; //등비수열
+	int endRowNum=pageNum*PAGE_ROW_COUNT; //등비수열 
 	
-	//하단 시작 페이지 번호 (정수를 정수로 나누면 소수점이 버려진 정수가 나온다) pageNum에 0~5를 넣어도 0.2 -> 0 +1 = 1로 유지가 됨.
+	//하단 시작 페이지 번호 (정수를 정수로 나누면 소수점이 버려진 정수가 나온다)
 	int startPageNum = 1 + ((pageNum-1)/PAGE_DISPLAY_COUNT)*PAGE_DISPLAY_COUNT;
 	//하단 끝 페이지 번호
 	int endPageNum=startPageNum+PAGE_DISPLAY_COUNT-1;
-	//전체 글의 갯수
-	int totalRow=BoardDao.getInstance().getCount();
-	//전체 페이지의 갯수 구하기 => double로 실수로 계산해서 정확한 소수점 
+	
+	/*
+		StringUtils 클래스의 isEmpty() static 메소드를 이용하면 문자열이 비었는지 여부를 알수 있다
+		null 또는 "" 빈문자열은  비었다고 판정된다.
+		
+		StringUtils.isEmpty(keyword)
+		는
+		keyword == null or "".equals(keyword) 
+		를 대체할수 있다.
+	*/
+	//전체 글의 갯수 
+	int totalRow=0;
+	//만일 전달된 keyword 가 없다면 
+	if(StringUtils.isEmpty(keyword)){
+		totalRow=BoardDao.getInstance().getCount();
+	}else{ //있다면 
+		totalRow=BoardDao.getInstance().getCountByKeyword(keyword);
+	}
+	
+	//전체 페이지의 갯수 구하기
 	int totalPageCount=(int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
 	//끝 페이지 번호가 이미 전체 페이지 갯수보다 크게 계산되었다면 잘못된 값이다.
 	if(endPageNum > totalPageCount){
 		endPageNum=totalPageCount; //보정해 준다. 
-	}
+	}	
 	
-	//dto 에 select 할 row 의 정보를 담고
+	//dto 에 select 할 row 의 정보를 담고 
 	BoardDto dto=new BoardDto();
 	dto.setStartRowNum(startRowNum);
 	dto.setEndRowNum(endRowNum);
+	//글목록 
+	List<BoardDto> list=null;
+	//만일 keyword 가 없아면 
+	if(StringUtils.isEmpty(keyword)){
+		list = BoardDao.getInstance().selectPage(dto);
+	}else{//있다면
+		// dto 에 keyword 를 담고 
+		dto.setKeyword(keyword);
+		// 키워드에 해당하는 글 목록을 얻어낸다 
+		list = BoardDao.getInstance().selectPageByKeyword(dto);
+	}
 	
-	//해당 row 만 select 한다
-	List<BoardDto> list = BoardDao.getInstance().selectPage(dto);
-
+	
 	//글목록 얻어오기
-	//List<BoardDto> list=BoardDao.getInstance().selectAll();
+	//List<BoardDto> list = BoardDao.getInstance().selectAll();
 
 %>
 <!DOCTYPE html>
@@ -56,66 +90,81 @@
 <head>
 <meta charset="UTF-8">
 <title>/board/list.jsp</title>
-
 <jsp:include page="/WEB-INF/include/resource.jsp"></jsp:include>
 </head>
 <body>
-   <jsp:include page="/WEB-INF/include/navbar.jsp">
-      <jsp:param value="board" name="thisPage"/>
-   </jsp:include>
-   <div class="container pt-1">
-      <a class="btn btn-outline-primary btn-sm" href="new-form.jsp">
-      새 글 작성 <i class="bi bi-pencil-square"></i>
-      </a>
-      <h1>게시글 목록 입니다</h1>
-      <table class="table table-bordered">
-         <thead>
-            <tr> 
-               <th>글번호</th>
-               <th>작성자</th>
-               <th>제목</th>
-               <th>조회수</th>
-               <th>작성일</th>
-            </tr>
-         </thead>
-         <tbody>
-         <%for(BoardDto tmp:list){ %>
-         	<tr>
-         		<td><%=tmp.getNum() %></td>
-         		<td><%=tmp.getWriter() %></td>
-         		<td>
-         			<a href="view.jsp?num=<%=tmp.getNum() %>"><%=tmp.getTitle() %></a>
-         		</td>
-         		<td><%=tmp.getViewCount() %></td>
-         		<td><%=tmp.getCreatedAt() %></td>
-         	</tr>
-         <%} %>   
-         </tbody>
-      </table>
-      
-      <ul class="pagination">
-      	<%-- startPageNum 이 1이 아닐때 이전 page 가 존재하기 때문에... --%>
-      	<%-- prev 링크를 조건부로 출력 --%>
-      	<%if(startPageNum != 1 ){ %>
-   			<li class="page-item">
-      			<a class="page-link" href="list.jsp?pageNum=<%=startPageNum-1 %>">&lsaquo;</a>
-      		</li>   	   	
-      	<%} %>
-      	<%-- page 번호를 반복문 돌면서 출력 --%>
-      	<%for(int i=startPageNum; i<=endPageNum; i++){ %>
-      		<li class="page-item">
-      			<a class="page-link <%= i==pageNum ? "active":"" %>" href="list.jsp?pageNum=<%=i%>"><%=i %></a>
-      		</li>
-      	<%} %>
-      	<%-- endPageNum 이 totalPageCount 보다 작을때 다음 page 가 있다--%> 
-      	<%-- next 링크를 조건부로 출력 --%>
-      	<%if(endPageNum < totalPageCount ){ %>
-      		<li class="page-item">
-      			<a class="page-link" href="list.jsp?pageNum=<%=endPageNum+1 %>">&rsaquo;</a>
-      		</li>      	
-      	<%} %>
-      </ul>
-      
-   </div>
+	<jsp:include page="/WEB-INF/include/navbar.jsp">
+		<jsp:param value="board" name="thisPage"/>
+	</jsp:include>
+	
+	<div class="container pt-1">
+		<a class="btn btn-outline-primary btn-sm" href="new-form.jsp">
+			새글 작성
+			<i class="bi bi-pencil-square"></i>
+		</a>
+		<h1>게시글 목록 입니다.</h1>
+		<!-- 행을 만들고  -->
+		<div class="row my-3">
+			<!-- 4/12 인 칼럼을 만들고 margin-start:auto 를 부여해서 왼쪽 마진을 자동 부여한다 -->
+			<div class="col-lg-4 col-md-6 ms-auto">
+				<form action="list.jsp" method="get">
+					<div class="input-group">
+						<input value="<%=StringUtils.isEmpty(keyword) ? "":keyword %>" type="text" name="keyword" class="form-control" placeholder="검색어 입력..."/>
+						<button type="submit" class="btn btn-outline-secondary">검색</button>
+					</div>
+				</form>
+			</div>
+		</div>
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th>글번호</th>
+					<th>작성자</th>
+					<th>제목</th>
+					<th>조회수</th>
+					<th>작성일</th>
+				</tr>
+			</thead>
+			<tbody>
+			<%for(BoardDto tmp:list){ %>
+				<tr>
+					<td><%=tmp.getNum() %></td> 
+					<td><%=tmp.getWriter() %></td>
+					<td>
+						<a href="view.jsp?num=<%=tmp.getNum() %>"><%=tmp.getTitle() %></a>
+					</td>
+					<td><%=tmp.getViewCount() %></td>
+					<td><%=tmp.getCreatedAt() %></td>
+				</tr>
+			<%} %>	
+			</tbody>
+		</table>
+		
+		<ul class="pagination">
+			<%-- startPageNum 이 1이 아닐때 이전 page 가 존재하기 때문에... --%>
+			<%if(startPageNum != 1){ %>
+				<li class="page-item">
+					<%-- &keyword=<%=keyword%> 를 추가해서, keyword 가 페이지 번호가 달라질때에도 함께 따라가도록 추가 --%>
+					<a class="page-link" href="list.jsp?pageNum=<%=startPageNum-1 %>&keyword=<%=keyword%>">&lsaquo;</a>
+				</li>
+			<%} %>			
+			<%for(int i=startPageNum; i<=endPageNum ; i++){ %>
+				<li class="page-item">
+					<a class="page-link <%= i==pageNum ? "active":"" %>" href="list.jsp?pageNum=<%=i %>&keyword=<%=keyword%>"><%=i %></a>
+				</li>
+			<%} %>
+			<%-- endPageNum 이 totalPageCount 보다 작을때 다음 page 가 있다 --%>		
+			<%if(endPageNum < totalPageCount){ %>
+				<li class="page-item">
+					<a class="page-link" href="list.jsp?pageNum=<%=endPageNum+1 %>&keyword=<%=keyword%>">&rsaquo;</a>
+				</li>
+			<%} %>	
+		</ul>
+		
+	</div>
 </body>
 </html>
+
+
+
+
